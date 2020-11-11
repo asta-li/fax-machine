cd9ae6d59f821a424babad8693f3462e7f0fbd3a
Asta <74164885+asta-li@users.noreply.github.com>
2020-11-10 21:45:57 -0800

    Refactor FileSelector component

--- client/src/App.js
+++ client/src/App.js
@@ -4,30 +4,46 @@ import React from 'react';
 import logo from './logo.svg';
 import './App.css';
 
-     
-class FaxMachineApp extends React.Component {
+
+class FileSelector extends React.Component {
   constructor(props) {
     super(props);
-    this.state = { 
+    this.state = {
       selectedFile: null,
-      uploadedFile: 'Please select a file',
+      selectedFileStatus: 'Select a file',
+      uploadedFileStatus: '',
     };
-    
     this.handleFileUpload = this.handleFileUpload.bind(this);
   }
 
-  // Handles file selection.
-  handleFileSelection(event) {
+  handleFileSelection(event) {  
+    const selectedFile = event.target.files[0];
     this.setState({
-      selectedFile: event.target.files[0],
-    }); 
-  } 
-     
-  // Handles file upload.
+      selectedFile: selectedFile,
+      selectedFileStatus: selectedFile.name,
+    });
+  }
+
+  renderSelectFile() {
+    return (
+      <div>
+        <label className='Select'>
+          <input type='file' onChange={(event) => this.handleFileSelection(event)} /> 
+          Select PDF
+        </label>
+        {this.state.selectedFileStatus}
+      </div>
+    );
+  }
+
   handleFileUpload() {
     console.log('Uploading', this.state.selectedFile);
 
-    if (this.state.selectedFile) {
+    if (!this.state.selectedFile) {
+      this.setState({
+        uploadedFileStatus: 'Please select a file to upload',
+      });
+    } else {
       const formData = new FormData(); 
       formData.append( 
         'fileToFax', 
@@ -40,38 +56,116 @@ class FaxMachineApp extends React.Component {
       axios.post('/upload', formData);
   
       this.setState({
-        uploadedFile: 'Uploaded!',
+        uploadedFileStatus: 'Uploaded!',
       });
+      this.props.setUploadedFile(this.state.selectedFile);
     }
-  } 
-     
-  render() {
-    let selectedFileStatus = 'Please select a file';
-    if (this.state.selectedFile) {
-      selectedFileStatus = this.state.selectedFile.name;
-    }
+  }
 
+  renderUploadFile() {
+    return (
+      <div>
+        <button className='Upload' onClick={this.handleFileUpload}>
+          Upload
+        </button>
+        {this.state.uploadedFileStatus}
+      </div>
+    );
+  }
+
+  render() {
+    return (
+      <div className='App-body'>
+        {this.renderSelectFile()}
+        {this.renderUploadFile()}
+      </div>
+    );
+  }
+}
+
+class FileFaxer extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      faxFileStatus: '',
+    };
+    this.handleFileFax = this.handleFileFax.bind(this);
+  }
+
+  handleFileFax() {
+    console.log('Faxing', this.props.uploadedFile);
+
+//    if (!this.props.uploadedFile) {
+//      this.setState({
+//        faxFileStatus: 'Upload a file to fax',
+//      });
+//    } else {
+//      const formData = new FormData(); 
+//      formData.append( 
+//        'fileToFax', 
+//        this.state.selectedFile, 
+//        this.state.selectedFile.name 
+//      ); 
+//       
+//      // Sends the file to the backend.
+//      // TODO(asta): Handle this request.
+//      axios.post('/upload', formData);
+  
+    this.setState({
+      faxFileStatus: 'Successfully faxed!',
+    });
+  }
+
+  renderFaxFile() {
+    return (
+      <div>
+        <button className='Upload' onClick={this.handleFileFax}>
+          Fax me!
+        </button>
+        {this.state.faxFileStatus}
+      </div>
+    );
+  }
+
+  render() {
+    return (
+      <div className='App-body'>
+        {this.renderFaxFile()}
+      </div>
+    );
+  }
+}
+
+class FaxMachineApp extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = { 
+      uploadedFile: null,
+    };
+    
+    this.setUploadedFile = this.setUploadedFile.bind(this);
+  }
+
+  // Handles file selection.
+  setUploadedFile(uploadedFile) {
+    this.setState({
+      uploadedFile: uploadedFile,
+    }); 
+  } 
+
+  render() {
     return (
       <div className='App'>
         <header className='App-header'>
           <img src={logo} className='App-logo' alt='logo' />
           <p>I am a fax machine.</p>
         </header>
-        <div className='App-body'> 
-          <div>
-            <label className='Select'>
-              <input type='file' onChange={(event) => this.handleFileSelection} /> 
-              Select PDF
-            </label>
-            {selectedFileStatus}
-          </div>
-          <div>
-            <button className='Upload' onClick={this.handleFileUpload}>
-              Upload now
-            </button>
-            {this.state.uploadedFile}
-          </div>
-        </div> 
+        <FileSelector
+          setUploadedFile={this.setUploadedFile}
+        />
+        <FileFaxer
+          uploadedFile={this.uploadedFile}
+        />
         <p>
           <a
             className='App-link'
