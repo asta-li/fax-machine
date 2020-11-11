4c323433bad66011a51da9d539c37b9e83dd4e22
Asta <74164885+asta-li@users.noreply.github.com>
2020-11-10 16:28:01 -0800

    Add file selection

--- client/src/App.js
+++ client/src/App.js
@@ -1,25 +1,80 @@
+import axios from 'axios'; 
+import React from 'react';
+
 import logo from './logo.svg';
 import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
+     
+class FaxMachineApp extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = { 
+      selectedFile: null,
+      uploadedFile: 'Please select a file',
+    };
+    
+    this.handleFileUpload = this.handleFileUpload.bind(this);
+  }
+
+  // Handles file selection.
+  handleFileSelection(event) {
+    this.setState({
+      selectedFile: event.target.files[0],
+    }); 
+  } 
+     
+  // Handles file upload.
+  handleFileUpload() {
+    console.log('Uploading', this.state.selectedFile);
+
+    if (this.state.selectedFile) {
+      const formData = new FormData(); 
+      formData.append( 
+        'fileToFax', 
+        this.state.selectedFile, 
+        this.state.selectedFile.name 
+      ); 
+       
+      // Sends the file to the backend.
+      // TODO(asta): Handle this request.
+      axios.post('/upload', formData);
+  
+      this.setState({
+        uploadedFile: 'Uploaded!',
+      });
+    }
+  } 
+     
+  render() {
+    return (
+      <div className='App'>
+        <header className='App-header'>
+          <img src={logo} className='App-logo' alt='logo' />
+          <p>I am a fax machine.</p>
+        </header>
+        <div className='App-body'> 
+          <label className='Select'>
+            <input type='file' onChange={(event) => this.handleFileSelection} /> 
+            Select PDF
+          </label>
+          <button className='Select' onClick={this.handleFileUpload}>
+            Upload now
+          </button>
+          {this.state.uploadedFile}
+        </div> 
         <p>
-          I am a fax machine.
+          <a
+            className='App-link'
+            href='https://github.com/asta-li/fax-machine'
+            target='_blank'
+            rel='noopener noreferrer'
+          >
+            Code
+          </a>
         </p>
-        <a
-          className="App-link"
-          href="https://github.com/asta-li/fax-machine"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Code
-        </a>
-      </header>
-    </div>
-  );
+      </div>
+    );
+  }
 }
 
-export default App;
+export default FaxMachineApp;
