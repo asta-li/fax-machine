28d3efe6835cc5fb8c4cf6925fedf118f6219cfe
Asta <74164885+asta-li@users.noreply.github.com>
2020-11-12 13:35:16 -0800

    Add write key-value pairs to GCS for state-keeping

--- server/fax.go
+++ server/fax.go
@@ -27,20 +27,27 @@ func uploadAndFaxFile(file *multipart.File, faxNumber string) error {
     log.Println("Uploaded file to", fileUrl)
 
     // Fax the file.
-    if err := faxUploadedFile(fileUrl, faxNumber); err != nil {
+    faxId, err := faxUploadedFile(fileUrl, faxNumber);
+    if err != nil {
+        return err
+    }
+    log.Println("Fax ID", faxId)
+
+    if err := writeState(bucketName, faxId, "key", "value"); err != nil {
         return err
     }
 
+    // TODO(asta): Move this to the fax completion handler.
     // Delete the file from GCS.
-    if err := deleteGCS(bucketName, fileName.String()); err != nil {
-        return err
-    }
-    log.Println("Deleted file at", fileUrl)
+    // if err := deleteGCS(bucketName, fileName.String()); err != nil {
+    //     return err
+    // }
+    // log.Println("Deleted file at", fileUrl)
 
     return nil
 }
 
-// Uploads an object to GCS.
+// Uploads a file to GCS.
 // See https://cloud.google.com/storage/docs/uploading-objects#storage-upload-object-go.
 // TODO(asta): Update permissions: https://cloud.google.com/storage/docs/access-control/signed-urls
 func uploadGCS(dataToWrite *multipart.File, bucketName string, fileName string) error {
@@ -59,6 +66,32 @@ func uploadGCS(dataToWrite *multipart.File, bucketName string, fileName string) 
     w := client.Bucket(bucketName).Object(fileName).NewWriter(ctx)
     if _, err = io.Copy(w, *dataToWrite); err != nil {
         return fmt.Errorf("io.Copy: %v", err)
+    }
+    if err := w.Close(); err != nil {
+        return fmt.Errorf("Writer.Close: %v", err)
+    }
+    return nil
+}
+
+// Write a (key, value) pair associated with the given faxId to GCS.
+func writeState(bucketName string, faxId string, key string, value string) error {
+    // TODO(asta): Pass in the client and context instead of regenerating each time.
+    // Create GCS connection
+    ctx := context.Background()
+    client, err := storage.NewClient(ctx)
+    if err != nil {
+        return fmt.Errorf("storage.NewClient: %v", err)
+    }
+    defer client.Close()
+
+    ctx, cancel := context.WithTimeout(ctx, time.Second*50)
+    defer cancel()
+
+    // Upload an object with storage.Writer.
+    fileName := faxId + "/" + key
+    w := client.Bucket(bucketName).Object(fileName).NewWriter(ctx)
+    if _, err := fmt.Fprintf(w, value); err != nil {
+        return fmt.Errorf("fmt.Fprintf to object: %v", err)
     }
     if err := w.Close(); err != nil {
         return fmt.Errorf("Writer.Close: %v", err)
@@ -88,7 +121,7 @@ func deleteGCS(bucketName string, fileName string) error {
 }
 
 // Fax a file via Telnyx Programmable Fax.
-func faxUploadedFile(fileUrl string, sendToNumber string) error {
+func faxUploadedFile(fileUrl string, sendToNumber string) (string, error) {
     // Set Telnyx account credentials.
     sendFromNumber := os.Getenv("FAX_FROM_NUMBER")
     appId := os.Getenv("TELNYX_APP_ID")
@@ -110,23 +143,24 @@ func faxUploadedFile(fileUrl string, sendToNumber string) error {
     urlStr := "https://api.telnyx.com/v2/faxes"
     req, err := http.NewRequest("POST", urlStr, &msgDataReader)
     if err != nil {
-        return err
+        return "", err
     }
     req.Header.Add("Authorization", bearer)
     //req.Header.Add("Accept", "application/json")
     req.Header.Add("Content-Type", "application/x-www-form-urlencoded")
     resp, err := client.Do(req)
     if err != nil {
-        return err
+        return "", err
     }
     defer resp.Body.Close()
 
     // Handle the response.
     if (resp.StatusCode < 200 || resp.StatusCode >= 300) {
-        return fmt.Errorf("Unsuccessful fax request", resp.Status)
+        return "", fmt.Errorf("Unsuccessful fax request", resp.Status)
     }
 
     // Record fax_id from response - this will be needed to match up the webhook response.
-    return nil
+    fax_id := "foo"
+    return fax_id, nil
 }
 
